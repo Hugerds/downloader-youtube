87b4ec05de8b348b6b60a3ecbdc3a7165b80cc8f
Hugo Esteves <hugo.esteves@unesp.br>
2020-10-10 16:15:27 -0300

    Update teste_video.py

--- teste_video.py
+++ teste_video.py
@@ -4,21 +4,31 @@ import pafy
 url = ("https://www.youtube.com/watch?v=ctQJpfuhoN8")
 video = pafy.new(url)
 streams = video.streams
-for s in streams:
-    print(s.resolution, s.extension, s.get_filesize(), s.url)
+#for s in streams:
+   # print(s.resolution, s.extension, s.get_filesize(), s.url)
 
-audiostreams = video.audiostreams
-for a in audiostreams:
-    print(a.bitrate, a.extension, a.get_filesize())
+#audiostreams = video.audiostreams
+#for a in audiostreams:
+ #   print(a.bitrate, a.extension, a.get_filesize())
+
+def mycb(total, recvd, ratio, rate, eta):
+    #print(recvd, ratio, eta)
+    print(f"Em andamento...\n")
 
 allstreams = video.allstreams
 for s in allstreams:
     print(s.mediatype, s.extension, s.quality)
 
+bestvideo = video.getbestvideo()
 bestaudio = video.getbestaudio()
-nome = video.title
-ext = str(input("Qual extensão?"))
-filename = bestaudio.download(filepath=f"D:\Downloads\Vídeos\{nome}.{ext}")
+tituloVideo = video.title
+op = str(input("Deseja baixar em audio ou video?\nSua opção: "))
+if "audio" in op:
+    filename = bestaudio.download(filepath=f"D:\Downloads\Vídeos\{tituloVideo}.mp3", meta=True, quiet=False, callback=mycb)
+    print(f"audio")
+if "video" in op:
+    filename = bestvideo.download(filepath=f"D:\Downloads\Vídeos\{tituloVideo}", meta=True, quiet=False, callback=mycb)
+    print(f"video")
 
 """"
 url = ("https://www.youtube.com/watch?v=ctQJpfuhoN8")
@@ -32,4 +42,3 @@ print(f"URL: {best.url}")
 #filename = audiostreams.download(filepath="D:\Downloads\Vídeos")
 filename = bestaudio.download(filepath="D:\Downloads\Vídeos")
 """
-
